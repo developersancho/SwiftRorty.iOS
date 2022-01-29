f0ee1eab71a835c3e9f86c3aee8b42001d4f47e8
developersancho <developersancho@gmail.com>
2022-01-30 00:28:56 +0300

    Initial Commit

---
+++ SwiftRorty.iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftRorty.iOS
+//
+//  Created by developersancho on 30.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftRorty.iOS/SwiftRorty_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRorty_iOSApp.swift
+//  SwiftRorty.iOS
+//
+//  Created by developersancho on 30.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRorty_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
