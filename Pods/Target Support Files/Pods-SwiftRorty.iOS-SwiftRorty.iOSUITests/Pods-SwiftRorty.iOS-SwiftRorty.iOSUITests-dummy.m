2dda5943076cffec71027514d5c65c55707112a7
Mr.Sanchez <developersancho@gmail.com>
2022-02-27 01:04:31 +0300

    ...
    ...

---
+++ Pods/Target Support Files/Pods-SwiftRorty.iOS-SwiftRorty.iOSUITests/Pods-SwiftRorty.iOS-SwiftRorty.iOSUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftRorty_iOS_SwiftRorty_iOSUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftRorty_iOS_SwiftRorty_iOSUITests
+@end
---
+++ Pods/Target Support Files/Resolver/Resolver-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Resolver : NSObject
+@end
+@implementation PodsDummy_Resolver
+@end
